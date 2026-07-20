df371c425aa97dab90aa87560c41b2b14d0f4a35
Fabian Yudan <112625042+fabianyudan@users.noreply.github.com>
2026-07-20 18:40:37 +0700

    Create app.py

---
+++ absa_models/app.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+BASE_MODEL_DIR = Path("absa_models")
+
+ASPECT_MODEL_DIR = BASE_MODEL_DIR / "aspect_extractor"
+SENTIMENT_MODEL_DIR = BASE_MODEL_DIR / "sentiment_classifier"
